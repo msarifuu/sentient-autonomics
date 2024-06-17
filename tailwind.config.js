3d97a810c1bb9d3268675013ce26f132028d9be5
Muhammad Sarif <msyarifu@gmail.com>
2024-06-17 21:10:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      'bai': 'Bai Jamjuree'
+    },
+    extend: {
+      colors: {
+        'primary': '#8CE100',
+        'secondary-dark': '#1F3300',
+        'secondary-light': '#406703',
+        'base-dark': '#000000',
+        'base-light': '#FFFFFF',
+        'accent': '#6A6969',
+        'floor': '#EFF8E8',
+      },
+    },
+  },
+  plugins: [],
+}
+
